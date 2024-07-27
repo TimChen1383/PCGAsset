259b69a85f9ee25a16d1a607c8e97eec89f9f3ff
TimChen <37008451+TimChen1383@users.noreply.github.com>
2024-07-27 18:03:34 +0100

    update cellular

--- Plugins/PCGAsset/Source/PCGAsset/Private/PCGCellularAutomata.cpp
+++ Plugins/PCGAsset/Source/PCGAsset/Private/PCGCellularAutomata.cpp
@@ -122,10 +122,10 @@ bool FPCGCellularAutomataElement::ExecuteInternal(FPCGContext* Context) const
 			TArray<FPCGPoint> TempOutputPoints = OutputPoints;
 			
 			//looping grids from left to right
-			for(int32 GridHeightPointCount = 1; GridHeightPointCount <= GridHeightPointCounts; GridHeightPointCount++)
+			for(int32 GridHeightPointCount = 0; GridHeightPointCount < GridHeightPointCounts; GridHeightPointCount++)
 			{
 				//looping grids from bottom to top
-				for(int32 GridWidthPointCount = 1; GridWidthPointCount <= GridWidthPointCounts; GridWidthPointCount++)
+				for(int32 GridWidthPointCount = 0; GridWidthPointCount < GridWidthPointCounts; GridWidthPointCount++)
 				{
 					//the counter that count the neighbor wall count
 					int32 NeighborWallCounts = 0;
@@ -145,11 +145,16 @@ bool FPCGCellularAutomataElement::ExecuteInternal(FPCGContext* Context) const
 							else
 							{
 								//Make sure it's not the center grid. If it's center grid of the checking bound, won't add any number to the neighbor counter
-								if(HeightCheckPoint != GridHeightPointCount || WidthCheckPoint != GridWidthPointCounts)
+								if(HeightCheckPoint == GridHeightPointCount && WidthCheckPoint == GridWidthPointCounts)
 								{
-									//Looks like this TempNum is more than the actual point counts
+									//Center check point do nothing
+								}
+								else
+								{
+									//The number is wrong. It's between 0-30 but we only have 25 points
+									//Try to print current point and it's neighbor points. Easier for debugging
 									int32 TempNum = (WidthCheckPoint*GridWidthPointCounts)+HeightCheckPoint;
-									UE_LOG(LogTemp, Warning, TEXT("The integer value is: %d"), TempNum);
+									UE_LOG(LogTemp, Warning, TEXT("Checking point : %d"), TempNum);
 
 									/**
 									if(TempNum >= 0 && TempNum <= TempOutputPoints.Num())
