6d01d73722950695e108f0c755873ee7ad4f5140
TimChen <37008451+TimChen1383@users.noreply.github.com>
2024-07-30 23:17:24 +0100

    update cellular

--- Plugins/PCGAsset/Source/PCGAsset/Private/PCGCellularAutomata.cpp
+++ Plugins/PCGAsset/Source/PCGAsset/Private/PCGCellularAutomata.cpp
@@ -198,19 +198,31 @@ bool FPCGCellularAutomataElement::ExecuteInternal(FPCGContext* Context) const
 					}
 					else if(NeighborWallCounts == 2)
 					{
-						OutputPoints[(GridHeightPointCount*GridHeightPointCounts) + GridWidthPointCount].Density = 1;
+						OutputPoints[(GridHeightPointCount*GridHeightPointCounts) + GridWidthPointCount].Density = 0;
 					}
 					else if(NeighborWallCounts == 3)
 					{
-						OutputPoints[(GridHeightPointCount*GridHeightPointCounts) + GridWidthPointCount].Density = 1;
+						OutputPoints[(GridHeightPointCount*GridHeightPointCounts) + GridWidthPointCount].Density = 0;
 					}
 					else if(NeighborWallCounts == 4)
 					{
-						OutputPoints[(GridHeightPointCount*GridHeightPointCounts) + GridWidthPointCount].Density = 0;
+						OutputPoints[(GridHeightPointCount*GridHeightPointCounts) + GridWidthPointCount].Density = 1;
+					}
+					else if(NeighborWallCounts == 5)
+					{
+						OutputPoints[(GridHeightPointCount*GridHeightPointCounts) + GridWidthPointCount].Density = 1;
+					}
+					else if(NeighborWallCounts == 6)
+					{
+						OutputPoints[(GridHeightPointCount*GridHeightPointCounts) + GridWidthPointCount].Density = 1;
+					}
+					else if(NeighborWallCounts == 7)
+					{
+						OutputPoints[(GridHeightPointCount*GridHeightPointCounts) + GridWidthPointCount].Density = 1;
 					}
 					else
 					{
-						OutputPoints[(GridHeightPointCount*GridHeightPointCounts) + GridWidthPointCount].Density = 0;
+						OutputPoints[(GridHeightPointCount*GridHeightPointCounts) + GridWidthPointCount].Density = 1;
 					}
 					
 					
