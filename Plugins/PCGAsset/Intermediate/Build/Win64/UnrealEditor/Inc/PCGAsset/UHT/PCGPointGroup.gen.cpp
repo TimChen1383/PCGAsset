79a6dedbb41deb333be60ddff60d01c72a207d17
TimChen <37008451+TimChen1383@users.noreply.github.com>
2024-08-17 13:34:45 +0100

    add point group node

---
+++ Plugins/PCGAsset/Intermediate/Build/Win64/UnrealEditor/Inc/PCGAsset/UHT/PCGPointGroup.gen.cpp
@@ -0,0 +1,127 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "PCGAsset/Public/PCGPointGroup.h"
+#include "PCG/Public/Metadata/PCGMetadataTypesConstantStruct.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodePCGPointGroup() {}
+
+// Begin Cross Module References
+COREUOBJECT_API UScriptStruct* Z_Construct_UScriptStruct_FVector();
+PCG_API UClass* Z_Construct_UClass_UPCGSettings();
+PCG_API UScriptStruct* Z_Construct_UScriptStruct_FPCGMetadataTypesConstantStruct();
+PCGASSET_API UClass* Z_Construct_UClass_UPCGPointGroupSettings();
+PCGASSET_API UClass* Z_Construct_UClass_UPCGPointGroupSettings_NoRegister();
+UPackage* Z_Construct_UPackage__Script_PCGAsset();
+// End Cross Module References
+
+// Begin Class UPCGPointGroupSettings
+void UPCGPointGroupSettings::StaticRegisterNativesUPCGPointGroupSettings()
+{
+}
+IMPLEMENT_CLASS_NO_AUTO_REGISTRATION(UPCGPointGroupSettings);
+UClass* Z_Construct_UClass_UPCGPointGroupSettings_NoRegister()
+{
+	return UPCGPointGroupSettings::StaticClass();
+}
+struct Z_Construct_UClass_UPCGPointGroupSettings_Statics
+{
+#if WITH_METADATA
+	static constexpr UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+		{ "IncludePath", "PCGPointGroup.h" },
+		{ "ModuleRelativePath", "Public/PCGPointGroup.h" },
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_CustomOffset_MetaData[] = {
+		{ "Category", "Settings" },
+#if !UE_BUILD_SHIPPING
+		{ "Comment", "/*******************************************\n\x09Promote variables that will be shown in details panel\n\x09********************************************///Control the Perlin noise height and scale\n" },
+#endif
+		{ "ModuleRelativePath", "Public/PCGPointGroup.h" },
+		{ "PCG_Overridable", "" },
+#if !UE_BUILD_SHIPPING
+		{ "ToolTip", "Promote variables that will be shown in details panel\n******************************************//Control the Perlin noise height and scale" },
+#endif
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_AttributeTypes_MetaData[] = {
+		{ "Category", "Settings" },
+		{ "ModuleRelativePath", "Public/PCGPointGroup.h" },
+		{ "ShowOnlyInnerProperties", "" },
+	};
+	static constexpr UECodeGen_Private::FMetaDataPairParam NewProp_GroupName_MetaData[] = {
+		{ "Category", "Settings" },
+		{ "ModuleRelativePath", "Public/PCGPointGroup.h" },
+		{ "ShowOnlyInnerProperties", "" },
+	};
+#endif // WITH_METADATA
+	static const UECodeGen_Private::FStructPropertyParams NewProp_CustomOffset;
+	static const UECodeGen_Private::FStructPropertyParams NewProp_AttributeTypes;
+	static const UECodeGen_Private::FNamePropertyParams NewProp_GroupName;
+	static const UECodeGen_Private::FPropertyParamsBase* const PropPointers[];
+	static UObject* (*const DependentSingletons[])();
+	static constexpr FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<UPCGPointGroupSettings>::IsAbstract,
+	};
+	static const UECodeGen_Private::FClassParams ClassParams;
+};
+const UECodeGen_Private::FStructPropertyParams Z_Construct_UClass_UPCGPointGroupSettings_Statics::NewProp_CustomOffset = { "CustomOffset", nullptr, (EPropertyFlags)0x0010000000000005, UECodeGen_Private::EPropertyGenFlags::Struct, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(UPCGPointGroupSettings, CustomOffset), Z_Construct_UScriptStruct_FVector, METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_CustomOffset_MetaData), NewProp_CustomOffset_MetaData) };
+const UECodeGen_Private::FStructPropertyParams Z_Construct_UClass_UPCGPointGroupSettings_Statics::NewProp_AttributeTypes = { "AttributeTypes", nullptr, (EPropertyFlags)0x0010000000000005, UECodeGen_Private::EPropertyGenFlags::Struct, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(UPCGPointGroupSettings, AttributeTypes), Z_Construct_UScriptStruct_FPCGMetadataTypesConstantStruct, METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_AttributeTypes_MetaData), NewProp_AttributeTypes_MetaData) }; // 550202394
+const UECodeGen_Private::FNamePropertyParams Z_Construct_UClass_UPCGPointGroupSettings_Statics::NewProp_GroupName = { "GroupName", nullptr, (EPropertyFlags)0x0010000000000005, UECodeGen_Private::EPropertyGenFlags::Name, RF_Public|RF_Transient|RF_MarkAsNative, nullptr, nullptr, 1, STRUCT_OFFSET(UPCGPointGroupSettings, GroupName), METADATA_PARAMS(UE_ARRAY_COUNT(NewProp_GroupName_MetaData), NewProp_GroupName_MetaData) };
+const UECodeGen_Private::FPropertyParamsBase* const Z_Construct_UClass_UPCGPointGroupSettings_Statics::PropPointers[] = {
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UPCGPointGroupSettings_Statics::NewProp_CustomOffset,
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UPCGPointGroupSettings_Statics::NewProp_AttributeTypes,
+	(const UECodeGen_Private::FPropertyParamsBase*)&Z_Construct_UClass_UPCGPointGroupSettings_Statics::NewProp_GroupName,
+};
+static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_UPCGPointGroupSettings_Statics::PropPointers) < 2048);
+UObject* (*const Z_Construct_UClass_UPCGPointGroupSettings_Statics::DependentSingletons[])() = {
+	(UObject* (*)())Z_Construct_UClass_UPCGSettings,
+	(UObject* (*)())Z_Construct_UPackage__Script_PCGAsset,
+};
+static_assert(UE_ARRAY_COUNT(Z_Construct_UClass_UPCGPointGroupSettings_Statics::DependentSingletons) < 16);
+const UECodeGen_Private::FClassParams Z_Construct_UClass_UPCGPointGroupSettings_Statics::ClassParams = {
+	&UPCGPointGroupSettings::StaticClass,
+	nullptr,
+	&StaticCppClassTypeInfo,
+	DependentSingletons,
+	nullptr,
+	Z_Construct_UClass_UPCGPointGroupSettings_Statics::PropPointers,
+	nullptr,
+	UE_ARRAY_COUNT(DependentSingletons),
+	0,
+	UE_ARRAY_COUNT(Z_Construct_UClass_UPCGPointGroupSettings_Statics::PropPointers),
+	0,
+	0x001000A0u,
+	METADATA_PARAMS(UE_ARRAY_COUNT(Z_Construct_UClass_UPCGPointGroupSettings_Statics::Class_MetaDataParams), Z_Construct_UClass_UPCGPointGroupSettings_Statics::Class_MetaDataParams)
+};
+UClass* Z_Construct_UClass_UPCGPointGroupSettings()
+{
+	if (!Z_Registration_Info_UClass_UPCGPointGroupSettings.OuterSingleton)
+	{
+		UECodeGen_Private::ConstructUClass(Z_Registration_Info_UClass_UPCGPointGroupSettings.OuterSingleton, Z_Construct_UClass_UPCGPointGroupSettings_Statics::ClassParams);
+	}
+	return Z_Registration_Info_UClass_UPCGPointGroupSettings.OuterSingleton;
+}
+template<> PCGASSET_API UClass* StaticClass<UPCGPointGroupSettings>()
+{
+	return UPCGPointGroupSettings::StaticClass();
+}
+DEFINE_VTABLE_PTR_HELPER_CTOR(UPCGPointGroupSettings);
+UPCGPointGroupSettings::~UPCGPointGroupSettings() {}
+// End Class UPCGPointGroupSettings
+
+// Begin Registration
+struct Z_CompiledInDeferFile_FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPointGroup_h_Statics
+{
+	static constexpr FClassRegisterCompiledInInfo ClassInfo[] = {
+		{ Z_Construct_UClass_UPCGPointGroupSettings, UPCGPointGroupSettings::StaticClass, TEXT("UPCGPointGroupSettings"), &Z_Registration_Info_UClass_UPCGPointGroupSettings, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(UPCGPointGroupSettings), 1337762279U) },
+	};
+};
+static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPointGroup_h_1931142993(TEXT("/Script/PCGAsset"),
+	Z_CompiledInDeferFile_FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPointGroup_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPointGroup_h_Statics::ClassInfo),
+	nullptr, 0,
+	nullptr, 0);
+// End Registration
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Plugins/PCGAsset/Intermediate/Build/Win64/UnrealEditor/Inc/PCGAsset/UHT/PCGPointGroup.generated.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "PCGPointGroup.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PCGASSET_PCGPointGroup_generated_h
+#error "PCGPointGroup.generated.h already included, missing '#pragma once' in PCGPointGroup.h"
+#endif
+#define PCGASSET_PCGPointGroup_generated_h
+
+#define FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPointGroup_h_14_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUPCGPointGroupSettings(); \
+	friend struct Z_Construct_UClass_UPCGPointGroupSettings_Statics; \
+public: \
+	DECLARE_CLASS(UPCGPointGroupSettings, UPCGSettings, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/PCGAsset"), NO_API) \
+	DECLARE_SERIALIZER(UPCGPointGroupSettings)
+
+
+#define FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPointGroup_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UPCGPointGroupSettings(UPCGPointGroupSettings&&); \
+	UPCGPointGroupSettings(const UPCGPointGroupSettings&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UPCGPointGroupSettings); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UPCGPointGroupSettings); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UPCGPointGroupSettings) \
+	NO_API virtual ~UPCGPointGroupSettings();
+
+
+#define FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPointGroup_h_10_PROLOG
+#define FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPointGroup_h_14_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPointGroup_h_14_INCLASS_NO_PURE_DECLS \
+	FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPointGroup_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> PCGASSET_API UClass* StaticClass<class UPCGPointGroupSettings>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPointGroup_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
