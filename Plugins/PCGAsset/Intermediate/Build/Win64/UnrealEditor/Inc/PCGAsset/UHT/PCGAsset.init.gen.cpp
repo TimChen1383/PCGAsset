514e77bbea8ea7219aa1605821744bdb52035581
TimChen <37008451+TimChen1383@users.noreply.github.com>
2024-10-12 14:49:47 +0100

    add new Rotate Around Center node

--- Plugins/PCGAsset/Intermediate/Build/Win64/UnrealEditor/Inc/PCGAsset/UHT/PCGAsset.init.gen.cpp
+++ Plugins/PCGAsset/Intermediate/Build/Win64/UnrealEditor/Inc/PCGAsset/UHT/PCGAsset.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodePCGAsset_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xFCE96F1D,
-				0x9B77F685,
+				0xAED0A351,
+				0x640C58C2,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_PCGAsset.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_PCGAsset.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_PCGAsset(Z_Construct_UPackage__Script_PCGAsset, TEXT("/Script/PCGAsset"), Z_Registration_Info_UPackage__Script_PCGAsset, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xFCE96F1D, 0x9B77F685));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_PCGAsset(Z_Construct_UPackage__Script_PCGAsset, TEXT("/Script/PCGAsset"), Z_Registration_Info_UPackage__Script_PCGAsset, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xAED0A351, 0x640C58C2));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
