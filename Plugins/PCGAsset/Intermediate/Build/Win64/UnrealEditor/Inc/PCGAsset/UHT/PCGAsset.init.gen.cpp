6877c0977cf99c33946f5046a207d7939caa24c4
TimChen <37008451+TimChen1383@users.noreply.github.com>
2024-10-23 16:02:42 +0100

    add Pile Up node

--- Plugins/PCGAsset/Intermediate/Build/Win64/UnrealEditor/Inc/PCGAsset/UHT/PCGAsset.init.gen.cpp
+++ Plugins/PCGAsset/Intermediate/Build/Win64/UnrealEditor/Inc/PCGAsset/UHT/PCGAsset.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodePCGAsset_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xD0C6B06D,
-				0x6AFA09BB,
+				0xA9C51CF1,
+				0xDC20F57B,
 				METADATA_PARAMS(0, nullptr)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_PCGAsset.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_PCGAsset.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_PCGAsset(Z_Construct_UPackage__Script_PCGAsset, TEXT("/Script/PCGAsset"), Z_Registration_Info_UPackage__Script_PCGAsset, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xD0C6B06D, 0x6AFA09BB));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_PCGAsset(Z_Construct_UPackage__Script_PCGAsset, TEXT("/Script/PCGAsset"), Z_Registration_Info_UPackage__Script_PCGAsset, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xA9C51CF1, 0xDC20F57B));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Plugins/PCGAsset/Intermediate/Build/Win64/UnrealEditor/Inc/PCGAsset/UHT/PCGPileUp.generated.h
@@ -0,0 +1,54 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "PCGPileUp.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PCGASSET_PCGPileUp_generated_h
+#error "PCGPileUp.generated.h already included, missing '#pragma once' in PCGPileUp.h"
+#endif
+#define PCGASSET_PCGPileUp_generated_h
+
+#define FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPileUp_h_13_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUPCGPileUpSettings(); \
+	friend struct Z_Construct_UClass_UPCGPileUpSettings_Statics; \
+public: \
+	DECLARE_CLASS(UPCGPileUpSettings, UPCGSettings, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/PCGAsset"), NO_API) \
+	DECLARE_SERIALIZER(UPCGPileUpSettings)
+
+
+#define FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPileUp_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	UPCGPileUpSettings(UPCGPileUpSettings&&); \
+	UPCGPileUpSettings(const UPCGPileUpSettings&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UPCGPileUpSettings); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UPCGPileUpSettings); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(UPCGPileUpSettings) \
+	NO_API virtual ~UPCGPileUpSettings();
+
+
+#define FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPileUp_h_9_PROLOG
+#define FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPileUp_h_13_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPileUp_h_13_INCLASS_NO_PURE_DECLS \
+	FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPileUp_h_13_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> PCGASSET_API UClass* StaticClass<class UPCGPileUpSettings>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_UnrealProjects_PCGDemo_Plugins_PCGAsset_Source_PCGAsset_Public_PCGPileUp_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
